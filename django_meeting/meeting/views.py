7b51cc1a8693e39d16d25c96c8a350704cf76c90
somya.hcode <somya.gupta@hcode.tech>
2024-10-23 13:55:08 +0530

    add home page css and dynamic var from views

--- django_meeting/meeting/views.py
+++ django_meeting/meeting/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse # Create your views here.
+from django.http import HttpResponse  # Create your views here.
+
 
 def index(request):
     meetups = [
-        {"title": "first meetup"},
-        {"title" : "second meetup"}
+        {"title": "first meetup", "location": "Paris", "slug": "a-first-meetup"},
+        {"title": "second meetup", "location": "New York", "slug": "a-second-meetup"},
     ]
 
     return render(request, ("meeting/index.html"), {
-        "show_meetup" : False,
-        "meetup" : meetups
+        "show_meetup": False,
+        "meetup": meetups
     })
