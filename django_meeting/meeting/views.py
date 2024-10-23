82b76b3a4a24d338ab1dac4d5447e6afc98268a8
somya.hcode <somya.gupta@hcode.tech>
2024-10-23 11:24:15 +0530

    add views data to index.html

--- django_meeting/meeting/views.py
+++ django_meeting/meeting/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse # Create your views here.
 
 def index(request):
-    return render(request, ("meeting/index.html"))
+    meetups = [
+        {"title": "first meetup"},
+        {"title" : "second meetup"}
+    ]
+
+    return render(request, ("meeting/index.html"), {
+        "show_meetup" : False,
+        "meetup" : meetups
+    })
